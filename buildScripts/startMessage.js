3ffc0b9d300b83884fe6806a4ea47507f3fdfbfd
Sam Ruby <samueljosephruby@gmail.com>
2017-11-20 03:53:32 +0000

    Added message to start scripts pointing to CI server

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,5 +1,5 @@
 import chalk from 'chalk'
 
 console.log(chalk.green("Attempting to start server..."))// eslint-disable-line no-console
-
 console.log(chalk.green("In C9 that means you have to go to: " +  'http://createstuff-sjruby.c9users.io/'))// eslint-disable-line no-console
+console.log(chalk.yellow("Rember to check the CI build when you push changes at: https://codeship.com/"))// eslint-disable-line no-console
