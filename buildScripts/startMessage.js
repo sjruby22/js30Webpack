a247d06c7b56c9e6b93c7dcba24f3edcb0425ad7
Sam Ruby <samueljosephruby@gmail.com>
2017-11-20 03:55:42 +0000

    cleaned up the eslint disable notes

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,5 +1,6 @@
 import chalk from 'chalk'
 
-console.log(chalk.green("Attempting to start server..."))// eslint-disable-line no-console
-console.log(chalk.green("In C9 that means you have to go to: " +  'http://createstuff-sjruby.c9users.io/'))// eslint-disable-line no-console
-console.log(chalk.yellow("Rember to check the CI build when you push changes at: https://codeship.com/"))// eslint-disable-line no-console
+/* eslint-disable no-console */
+console.log(chalk.green("Attempting to start server..."))
+console.log(chalk.green("In C9 that means you have to go to: " +  'http://createstuff-sjruby.c9users.io/'))
+console.log(chalk.yellow("Rember to check the CI build when you push changes at: https://codeship.com/"))
